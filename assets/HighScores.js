6a9eada6c60bf7da4df4b5ca4da51e0c521d4aff
Baofeng Guo <peterguo1983@gmail.com>
2022-01-04 15:24:25 -0800

    Add comments to the HighScores.js.

--- assets/HighScores.js
+++ assets/HighScores.js
@@ -2,7 +2,7 @@ var highScore = document.querySelector("#highScore");
 var clear = document.querySelector("#clear");
 var goBack = document.querySelector("#goBack");
 
-
+// Add event listener for Clear button
 clear.addEventListener("click", function () {
     localStorage.clear();
     location.reload();
@@ -18,7 +18,7 @@ if (allScores !== null) {
         highScore.appendChild(createLi);
     }
 }
-
+// Add event listener for Go Back button
 goBack.addEventListener("click", function () {
     window.location.replace("./index.html");
 });
